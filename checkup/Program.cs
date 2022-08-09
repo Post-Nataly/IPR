39b55fafba6337d30f0462e14f8db51ba2becfb2
unknown <post-design@mail.ru>
2022-08-09 22:50:01 +0800

    программа

--- checkup/Program.cs
+++ checkup/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿static void Sorted(string[] any)
+{
+    for (int i = 0; i < any.Length; i++)
+    {
+        string a = any[i];
+        char[] sumstring = a.ToCharArray();
+        if (sumstring.Length <= 3)
+        {
+            string newSymbols = new string (sumstring);
+            Console.Write(newSymbols);
+            Console.Write(", ");
+        }
+    }
+    Console.Write("\b \b");
+    Console.Write("\b \b");
+}        
+
+string[] symbols = {"hello", "2", "world", ":-)", "наконец-то", "пол", "учи", "лос", "ь!!", "?"};
+Sorted(symbols);
